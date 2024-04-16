8fff438988bce896b3d75c73b41230b48cea4074
Dasha <dasaselehina@gmail.com>
2024-04-16 13:17:20 +0300

    Сделала по красивше вывод счета

--- Finansy/Form1.cs
+++ Finansy/Form1.cs
@@ -27,19 +27,19 @@ namespace Finansy
         {
             add = Convert.ToInt32(maskedTextBox1.Text);
             account = account + add;
-            MessageBox.Show($"ƒенег на счете: {account}");
+            MessageBox.Show($"ƒенег на счете: {account}", "—чет", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void but_subtr_Click(object sender, EventArgs e)
         {
             subtr = Convert.ToInt32(maskedTextBox2.Text);
             account = account - subtr;
-            MessageBox.Show($"ƒенег на счете: {account}");
+            MessageBox.Show($"ƒенег на счете: {account}", "—чет", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void bank_account_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"ƒенег на счете: {account}");
+            MessageBox.Show($"ƒенег на счете: {account}", "—чет", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
